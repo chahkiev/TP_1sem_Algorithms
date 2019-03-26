30989740db12d993f9e7fb4ad6f24db03c20e9f1
chahkiev <06aga06m@mail.ru>
2019-03-26 19:18:01 +0300

    added comparison of 'realloc' and 'new'

---
+++ sravnenie/cmake-build-debug/CMakeFiles/sravnenie.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sravnenie.dir/main.cpp.obj"
+  "sravnenie.pdb"
+  "sravnenie.exe"
+  "sravnenie.exe.manifest"
+  "libsravnenie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sravnenie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
